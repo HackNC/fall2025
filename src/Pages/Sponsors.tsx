d851653d388143c1b8195365c151f2727841e352
Alicia <aliciasbao@gmail.com>
2025-08-24 15:43:33 -0400

    merge w nav

--- src/Pages/Sponsors.tsx
+++ src/Pages/Sponsors.tsx
@@ -1,45 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState, useEffect } from 'react';
+import { makeIsMobileState, MAX_MOBILE_WIDTH } from '../Main Website/Utils';
 
 const SponsorshipPage: React.FC = () => {
     var sponsors = import.meta.glob('/public/sponsors/*.{png,jpg,jpeg,svg}', { eager: true });
+    const [isMobile, setIsMobile] = useState(window.innerWidth < MAX_MOBILE_WIDTH);
 
-    console.log(sponsors)
+    var gridClass = isMobile ? 'grid grid-cols-1 gap-x-8 gap-y-4' : 'grid grid-cols-3 gap-x-8'
+    var divClass = isMobile ? 'px-20 text-center' : 'px-40 text-center' 
+    var imgClass = isMobile ? 'w-[60%] h-auto p-5 object-contain': 'w-full h-auto px-8 py-5 object-contain'
+    
+
+    useEffect(() => {
+        const handleResize = (()=> {
+            setIsMobile(window.innerWidth < MAX_MOBILE_WIDTH);
+        });
+        window.addEventListener('resize', handleResize);
+        handleResize();
+        return () => window.removeEventListener('resize', handleResize);
+    });
+
 
     return (
-        <div id="SponsorshipPage" className="px-40 text-center">
+        <div id="SponsorshipPage" className={divClass}>
             <h1 className="text-[#F6B1B1] text-shadow-font-pink">Past Sponsors</h1>
-            <div className='grid sm:grid-cols-1 lg:grid-cols-3 grid-auto-col gap-x-8 sm:gap-y-4'>
-                <div className="bg-font-orange bg-opacity-50 rounded-xl shadow-font-orange shadow-md">
+            <div className={gridClass}>
+                <div className="flex flex-col items-center bg-font-orange bg-opacity-50 rounded-xl shadow-font-orange shadow-md">
                     {Object.values(sponsors).map((logo: any, index: number) => (
                        index % 3 === 0 ? (
                         <img
                             key={index}
                             src={logo.default}
                             alt={`Sponsor ${index + 1}`}
-                            className="w-full h-auto px-10 py-5 object-contain"
+                            className={imgClass}
                         />) : null
                     ))}
                 </div>
-                <div className="bg-font-orange bg-opacity-50 rounded-xl shadow-font-orange shadow-md">
+                <div className="flex flex-col items-center bg-font-orange bg-opacity-50 rounded-xl shadow-font-orange shadow-md">
                     {Object.values(sponsors).map((logo: any, index: number) => (
                        index % 3 === 1 ? (
                         <img
                             key={index}
                             src={logo.default}
                             alt={`Sponsor ${index + 1}`}
-                            className="w-full h-auto px-10 py-5 object-contain"
+                            className={imgClass}
                         />) : null
                     ))}
                 </div>
-                <div className="bg-font-orange bg-opacity-50 rounded-xl shadow-font-orange shadow-md">
+                <div className="flex flex-col items-center bg-font-orange bg-opacity-50 rounded-xl shadow-font-orange shadow-md">
                     {Object.values(sponsors).map((logo: any, index: number) => (
                        index % 3 === 2 ? (
                         <img
                             key={index}
                             src={logo.default}
                             alt={`Sponsor ${index + 1}`}
-                            className="w-full h-auto px-10 py-5 object-contain"
+                            className={imgClass}
                         />) : null
                     ))}
                 </div>
@@ -48,3 +62,7 @@ const SponsorshipPage: React.FC = () => {
     );
 }
 export default SponsorshipPage
+
+function getWindowDimensions() {
+    throw new Error('Function not implemented.');
+}
