1b07da055b65fbc56ad6538a9e3b70659234fb1c
Luca <143050991+lucajusts@users.noreply.github.com>
2025-09-20 19:43:44 -0400

    Add line break between date and location on mobile (front page)

--- src/Pages/FrontPage.tsx
+++ src/Pages/FrontPage.tsx
@@ -12,7 +12,7 @@ const FrontPage: React.FC<FrontPageProps> = ({ scrollToSection }) => {
             <div className="flex flex-col text-center items-center font-jersey tracking-widest"
                 style={{ gap: "2.5rems" }}>
                 <h1 className="text-9xl text-font-light-blue text-shadow-accent-blue">HACKNC 2025</h1>
-                <h3 className="text-2xl text-primary-light">Oct 10-12, 2025 • In-person at UNC Chapel Hill</h3>
+                <h3 className="text-2xl text-primary-light">Oct 10-12, 2025 • <br className="md:hidden" /> In-person at UNC Chapel Hill</h3>
                 <img src="./logos/hacknc_logo_no_text_ glow.svg" alt="hacknc 2025 logo" className="scale-x-[-1] max-w-[90%] max-h-[90%]" />
                 <div className="flex items-center justify-center xs:text-4xl sm:text-6xl md:text-7xl space-x-4">
                     <img src="./frontPageAssets/plain_arrow_pink.png" alt="pink arrow" className="max-w-[0.5em] max-h-[0.5em]" />
